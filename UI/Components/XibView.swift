bd8922810f30153be9f0614958f6cda0a4f47d7f
Mu-Sheng Wu <mu-sheng@skrumble.com>
2018-06-18 11:26:29 -0400

    Craeted a XibView that will use class name as its Xib file name.

---
+++ UI/Components/XibView.swift
@@ -0,0 +1,9 @@
+//
+//  XibView.swift
+//  UI
+//
+//  Created by Mu-Sheng Wu on 2018-06-18.
+//  Copyright © 2018 Mu-Sheng Wu. All rights reserved.
+//
+
+import Foundation
---
+++ UI/Extensions/UIStoryboard+Extension.swift
@@ -0,0 +1,9 @@
+//
+//  UIStoryboard+Extension.swift
+//  UI
+//
+//  Created by Mu-Sheng Wu on 2018-06-18.
+//  Copyright © 2018 Mu-Sheng Wu. All rights reserved.
+//
+
+import Foundation
